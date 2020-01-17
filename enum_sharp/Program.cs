669f7717df66c8b81bdd8ea8b5d1d27e2d0aa032
Andrey Martunyk <aniks.zoom@gmail.com>
2020-01-17 15:43:49 +0200

    task1 done

--- enum_sharp/Program.cs
+++ enum_sharp/Program.cs
@@ -4,17 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace struct_sharp
+namespace enum_sharp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Notebook note = new Notebook("bob-toy", "KingLion", 2323.32);
-            note.ShowNote();
-
-
-            Console.ReadKey();
         }
     }
 }
---
+++ struct_sharp/Notebook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace struct_sharp
+{
+    struct Notebook
+    {
+        private string _model;
+        private string _manufacturer;
+        private double _price;
+
+
+        public Notebook(string model, string manufacturer, double price)
+        {
+            _model = model;
+            _manufacturer = manufacturer;
+            _price = price;
+        }
+
+        public void ShowNote()
+        {
+            Console.WriteLine(String.Format("Model: {0};", _model));
+            Console.WriteLine(String.Format("Manufacturer: {0};", _manufacturer));
+            Console.WriteLine(String.Format("Price: {0};", _price));
+        }
+    }
+}
